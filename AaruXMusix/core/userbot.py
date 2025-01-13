e24b710c1720d5b9eb9d9976c5ccf766e00017bd
𝐒ʜɪᴠ 𝐒ɪɴɢʜ / 🚩 <mishuxd.19@gmail.com>
2025-01-13 17:56:44 +0530

    Update userbot.py

--- AaruXMusix/core/userbot.py
+++ AaruXMusix/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.one.join_chat("Dns_Official_Channel")
               
-                await self.one.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.one.join_chat("Dns_Official_Channel")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.two.join_chat("Dns_Official_Channel")
                 
-                await self.one.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.one.join_chat("Dns_Official_Channel")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.three.join_chat("Dns_Official_Channel")
                 
-                await self.one.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.one.join_chat("Dns_Official_Channel")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.four.join_chat("Dns_Official_Channel")
                 
-                await self.one.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.one.join_chat("Dns_Official_Channel")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.five.join_chat("Dns_Official_Channel")
                 
-                await self.one.join_chat("Mat_Dek_Bura_Man_Jye_Ga")
+                await self.one.join_chat("Dns_Official_Channel")
             except:
                 pass
             assistants.append(5)
