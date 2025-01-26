dba8c8009ff4e9f666e85338cd0a6ba79b344f8b
Dvis <gokhaledurgesh4@gmail.com>
2025-01-26 21:56:27 +0530

    Update unbanall.py

--- AaruXMusix/plugins/admins/unbanall.py
+++ AaruXMusix/plugins/admins/unbanall.py
@@ -2,7 +2,7 @@ from AaruXMusix import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from AaruXMusix.utils.Rudra_ban import admin_filter
+from AaruXMusix.utils.Aaru_ban import admin_filter
 
 BOT_ID = "6196153811"
 
