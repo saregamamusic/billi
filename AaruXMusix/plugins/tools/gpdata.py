3f664ea5b44f0395809c7164e5cf0724610a1c46
𝐒ʜɪᴠ 𝐒ɪɴɢʜ / 🚩 <mishuxd.19@gmail.com>
2025-01-27 09:55:53 +0530

    Update gpdata.py

--- AaruXMusix/plugins/tools/gpdata.py
+++ AaruXMusix/plugins/tools/gpdata.py
@@ -4,7 +4,7 @@ from pyrogram import filters, Client
 from AaruXMusix import app
 from config import OWNER_ID
 from pyrogram.types import Message
-from AaruXMusix.utils.Rudra_ban import admin_filter
+from AaruXMusix.utils.Aaru_ban import admin_filter
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 
