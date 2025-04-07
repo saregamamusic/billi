009206faa84457b387f15105e7b3a7ec24da22c4
saregamamusic <bittulkr628@gmail.com>
2025-04-07 22:39:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,15 +39,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
    "UPSTREAM_REPO",
-   "https://github.com/saregamamusic/rk",
+   "https://github.com/saregamamusic/billi",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
    "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Dns_Official_Channel")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DNS_NETWORK")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/btw_ghouls")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ghouls_here")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -85,10 +85,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-   "START_IMG_URL", "https://files.catbox.moe/2t62vg.jpg"
+   "START_IMG_URL", "https://graph.org/file/c1403435092ac78a9b00c-e2308add62f457f567.jpg"
 )
 PING_IMG_URL = getenv(
-   "PING_IMG_URL", "https://files.catbox.moe/yyc0e8.jpg"
+   "PING_IMG_URL", "https://graph.org/file/91e3a6be47104501699cc-f00851488c31dd7f5f.jpg"
 )
 PLAYLIST_IMG_URL = "https://files.catbox.moe/5yfzv2.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/5pkn9k.jpg"
